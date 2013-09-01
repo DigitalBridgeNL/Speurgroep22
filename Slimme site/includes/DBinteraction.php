21fc57e361dd9f92c17429083d9e99fd00cf6f3e
Koen en Peter <info@digitalbridge.nl>
2013-09-01 13:57:00 +0200

    FUncties
    Verschillende functies toegevoegd voor het vinden van de branches en
    specialiteiten.

--- Slimme site/includes/DBinteraction.php
+++ Slimme site/includes/DBinteraction.php
@@ -1,11 +1,5 @@
 <?php
   session_start();
-  $DB;
-  define("HOST", "db2.hosting2go.nl"); // The host you want to connect to.
-  define("USER", "m1_4ebf03ad"); // The database username.
-  define("PASSWORD", "4Lw3zYTx9S"); // The database password. 
-  define("DATABASE", "m1_4ebf03ad"); // The database name.
-  $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
   
 //----------------------------------------------------------------------Algemene functies
   function openDB()
@@ -17,6 +11,73 @@
       }
       mysql_select_db("m1_4ebf03ad", $DB) or die('Could not connect to the database. Database may not exist.' . mysql_error());
   }
+  
+  function branches()
+      	{
+			openDB();
+           $result = mysql_query("
+           select brancheID, naam
+           from branche order by naam");
+
+           return $result;
+       }
+	   
+  function current_branche($user)
+  {
+	openDB();
+	$result = mysql_query("
+           select brancheID
+           from user where userID = '$user'"); 
+	return $result;
+	  
+  }
+  
+  function updatePakket3($pakket, $user, $contactpersoon, $bedrijfsnaam, $adres, $postcode, $plaats, $website, $email)
+  {
+	openDB();
+	// update naw gegevens
+	mysql_query("UPDATE bedrijf SET contactpersoon = '$contactpersoon', bedrijfsnaam = '$bedrijfsnaam', adres = '$adres', postcode = '$postcode', plaats = '$plaats', website = '$website', email = '$email' WHERE userid = '$user'");
+	 //update pakketkeuze + profiel foto
+	mysql_query("UPDATE user SET pakketid = '$pakket' WHERE userid = '$user'");
+  }
+  
+  function updatePakket2($user, $contactpersoon, $bedrijfsnaam, $adres, $postcode, $plaats, $website, $email, $telefoonnummer1, $telefoonnummer2, $profielfoto)
+  {
+	openDB();
+	//update naw gegevens + telefoonnummers
+	mysql_query("UPDATE bedrijf SET contactpersoon = '$contactpersoon', bedrijfsnaam = '$bedrijfsnaam', adres = '$adres', postcode = '$postcode', plaats = '$plaats', website = '$website', email = '$email', telefoonnummer1 = '$telefoonnummer1', telefoonnummer2 = '$telefoonnummer2' WHERE userid = '$user'");
+	 //update pakketkeuze + profiel foto
+	mysql_query("UPDATE user SET profielfoto = '$profielfoto', $pakketid = '$pakket' WHERE userid = '$user'");
+  }
+  
+  function updatePakket1($pakket, $user,$contactpersoon,$bedrijfsnaam,$adres,$postcode,$plaats,$website,$email, $telefoonnummer1, $telefoonnummer2, $profielfoto, $foto1, $foto2, $foto3, $foto4, $foto5, $foto6, $foto7, $foto8) 
+  {
+	 openDB();
+	 //update naw gegevens + telefoonnummers
+	 mysql_query("UPDATE bedrijf SET contactpersoon = '$contactpersoon', bedrijfsnaam = '$bedrijfsnaam', adres = '$adres', postcode = '$postcode', plaats = '$plaats', website = '$website', email = '$email', telefoonnummer1 = '$telefoonnummer1', telefoonnummer2 = '$telefoonnummer2' WHERE userid = '$user'");
+	 //update pakketkeuze + profiel foto
+	mysql_query("UPDATE user SET profielfoto = '$profielfoto', $pakketid = '$pakket' WHERE userid = '$user'");
+	 //update foto`s
+	mysql_query("INSERT INTO user_foto (userid, foto1, foto2, foto3, foto4, foto5, foto6, foto7, foto8) VALUES ('$user',$foto1, $foto2, $foto3, $foto4, $foto5, $foto6, $foto7, $foto8)");
+	  
+  }
+  
+  function specialisaties($currentbranche)
+  {
+	openDB();
+	$result = mysql_query("
+           select bs_id, specialisatie
+           from branche_specialisatie where brancheID = '$currentbranche'"); 
+	return $result;	  
+  }
+  
+  function get_users_details($user)
+  {	 
+	openDB();
+	$result = mysql_query("SELECT b.contactpersoon, b.bedrijfsnaam, b.adres, b.postcode, b.plaats, b.website, b.email, b.telefoonnummer1, b.telefoonnummer2, u.brancheID FROM bedrijf b, user u WHERE u.userID = b.userid AND u.userID = '$user'");
+	
+	return $result;
+ }
 
   function closeDB()
   {
@@ -155,124 +216,6 @@
  }
  
  
- function sec_session_start() {
-        $session_name = 'sec_session_id'; // Set a custom session name
-        $secure = false; // Set to true if using https.
-        $httponly = true; // This stops javascript being able to access the session id. 
- 
-        ini_set('session.use_only_cookies', 1); // Forces sessions to only use cookies. 
-        $cookieParams = session_get_cookie_params(); // Gets current cookies params.
-        session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly); 
-        session_name($session_name); // Sets the session name to the one set above.
-        session_start(); // Start the php session
-        session_regenerate_id(); // regenerated the session, delete the old one.  
-}
-
-
-function login($email, $password, $mysqli) {
-   // Using prepared Statements means that SQL injection is not possible. 
-   if ($stmt = $mysqli->prepare("SELECT userID, username, password, salt FROM user WHERE username = ? LIMIT 1")) { 
-      $stmt->bind_param('s', $email); // Bind "$email" to parameter.
-      $stmt->execute(); // Execute the prepared query.
-      $stmt->store_result();
-      $stmt->bind_result($user_id, $username, $db_password, $salt); // get variables from result.
-      $stmt->fetch();
-      $password = hash('sha512', $password.$salt); // hash the password with the unique salt.
- 
-      if($stmt->num_rows == 1) { // If the user exists
-         // We check if the account is locked from too many login attempts
-         if(checkbrute($user_id, $mysqli) == true) { 
-            // Account is locked
-            // Send an email to user saying their account is locked
-            return false;
-         } else {
-         if($db_password == $password) { // Check if the password in the database matches the password the user submitted. 
-            // Password is correct!
- 
- 
-               $user_browser = $_SERVER['HTTP_USER_AGENT']; // Get the user-agent string of the user.
- 
-               $user_id = preg_replace("/[^0-9]+/", "", $user_id); // XSS protection as we might print this value
-               $_SESSION['user_id'] = $user_id; 
-               $username = preg_replace("/[^a-zA-Z0-9_\-]+/", "", $username); // XSS protection as we might print this value
-               $_SESSION['username'] = $username;
-               $_SESSION['login_string'] = hash('sha512', $password.$user_browser);
-               // Login successful.
-               return true;    
-         } else {
-            // Password is not correct
-            // We record this attempt in the database
-            $now = time();
-            $mysqli->query("INSERT INTO login_attempts (user_id, time) VALUES ('$user_id', '$now')");
-            return false;
-         }
-      }
-      } else {
-         // No user exists. 
-         return false;
-      }
-   }
-}
-
-function checkbrute($user_id, $mysqli) {
-   // Get timestamp of current time
-   $now = time();
-   // All login attempts are counted from the past 2 hours. 
-   $valid_attempts = $now - (2 * 60 * 60); 
- 
-   if ($stmt = $mysqli->prepare("SELECT time FROM login_attempts WHERE user_id = ? AND time > '$valid_attempts'")) { 
-      $stmt->bind_param('i', $user_id); 
-      // Execute the prepared query.
-      $stmt->execute();
-      $stmt->store_result();
-      // If there has been more than 5 failed logins
-      if($stmt->num_rows > 5) {
-         return true;
-      } else {
-         return false;
-      }
-   }
-}
-
-
-function login_check($mysqli) {
-   // Check if all session variables are set
-   if(isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['login_string'])) {
-     $user_id = $_SESSION['user_id'];
-     $login_string = $_SESSION['login_string'];
-     $username = $_SESSION['username'];
- 
-     $user_browser = $_SERVER['HTTP_USER_AGENT']; // Get the user-agent string of the user.
- 
-     if ($stmt = $mysqli->prepare("SELECT password FROM user WHERE id = ? LIMIT 1")) { 
-        $stmt->bind_param('i', $user_id); // Bind "$user_id" to parameter.
-        $stmt->execute(); // Execute the prepared query.
-        $stmt->store_result();
- 
-        if($stmt->num_rows == 1) { // If the user exists
-           $stmt->bind_result($password); // get variables from result.
-           $stmt->fetch();
-           $login_check = hash('sha512', $password.$user_browser);
-           if($login_check == $login_string) {
-              // Logged In!!!!
-              return true;
-           } else {
-              // Not logged in
-              return false;
-           }
-        } else {
-            // Not logged in
-            return false;
-        }
-     } else {
-        // Not logged in
-        return false;
-     }
-   } else {
-     // Not logged in
-     return false;
-   }
-}
 
   
   
